e18ca2815e4d7061403a774bcdb15c905db705a3
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2020-08-10 15:04:09 -0300

    using pattern match

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -5,8 +5,8 @@ defmodule FizzBuzz do
   torcar multiplos de 5 por 'Buzz'
   trocar todos multiplos de 3 e 5 por 'FizzBuzz'
   """
-  def build(file_naame) do
-    file = File.read(file_naame)
-    IO.inspect(file)
+  def build(file_name) do
+    {:ok, file} = File.read(file_name)
+    file
   end
 end
