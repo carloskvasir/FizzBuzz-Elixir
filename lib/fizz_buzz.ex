33030a2112197de1bb09e52984b0f7a48cb3e105
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2020-08-10 20:17:25 -0300

    refactor: just entrypoint not private

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -11,21 +11,22 @@ defmodule FizzBuzz do
     |> handle_file_read()
   end
 
-  def handle_file_read({:ok, result}) do
+  defp handle_file_read({:ok, result}) do
     result
     |> String.split(",")
     |> Enum.map(&convert_and_evaluate_numbers/1)
   end
 
-  def handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
+  defp handle_file_read({:error, reason}), do: "Error reading the file: #{reason}"
 
-  def convert_and_evaluate_numbers(elem) do
-    number = String.to_integer(elem)
-    evaluate_number(number)
+  defp convert_and_evaluate_numbers(elem) do
+    elem
+    |> String.to_integer()
+    |> evaluate_number()
   end
 
-  def evaluate_number(number) when rem(number, 3 * 5) == 0, do: :fizzbuzz
-  def evaluate_number(number) when rem(number, 3) == 0, do: :fizz
-  def evaluate_number(number) when rem(number, 5) == 0, do: :buzz
-  def evaluate_number(number), do: number
+  defp evaluate_number(number) when rem(number, 3 * 5) == 0, do: :fizzbuzz
+  defp evaluate_number(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_number(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_number(number), do: number
 end
