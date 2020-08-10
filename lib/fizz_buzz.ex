e7c8d9e24241e19effa89b7690e6d0ad435b7bb0
Carlos Lima (Kvasir) <carloskvasir@gmail.com>
2020-08-10 16:20:36 -0300

    refactor: using pipe operator

--- lib/fizz_buzz.ex
+++ lib/fizz_buzz.ex
@@ -6,8 +6,9 @@ defmodule FizzBuzz do
   trocar todos multiplos de 3 e 5 por 'FizzBuzz'
   """
   def build(file_name) do
-    file = File.read(file_name)
-    handle_file_read(file)
+    file_name
+    |> File.read()
+    |> handle_file_read()
   end
 
   def handle_file_read({:ok, result}), do: result
